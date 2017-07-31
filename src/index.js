d82e7f66c0d399ccecaed8731bb9038f5c4c7d4f
Dave Bell <david.bell@isotoma.com>
2017-07-31 14:56:13 +0100

    pass in group name

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const reducers = combineReducers({
 
 let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+config.group = 'admins';
 setupCognito(store, config);
 
 ReactDOM.render(
